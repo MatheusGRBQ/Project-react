056888c2266a40299aa6995d69c226039890e8aa
Matheus <matheus.grbq@gmail.com>
2021-03-28 10:51:18 -0300

    Enter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css"
-import NavBar from "../src/components/NavBar/NavBar.js"
 
 function App() {
   return (
     <div className="App">
+      <h1>Olá Mundo!</h1>
       <ul className="lista">
         <li>Vamos começar</li>
         <li>Linhas teste</li>
