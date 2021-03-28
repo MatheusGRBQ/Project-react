4f7383833df0464d98af5b495ebdb12ab0f83468
Matheus <matheus.grbq@gmail.com>
2021-03-28 15:53:14 -0300

    Enter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css"
+
 import Navbar from "./Components/Navbar/Navbar";
+import TaskList from "./Components/TaskList/TaskList"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <TaskList />
       <ul className="lista">
         <li>Vamos começar</li>
         <li>Linhas teste</li>
