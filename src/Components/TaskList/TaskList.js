bfabf6bba37526c2b95e4ec4f9aeb44ed07b0292
Matheus <matheus.grbq@gmail.com>
2021-03-28 22:49:28 -0300

    enter

--- src/Components/TaskList/TaskList.js
+++ src/Components/TaskList/TaskList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./TaskList.css";
 
-export default function TaskList(props){
+export default function TaskList({ title }){
     return(
         <div className="tasklist">
-        <div className="title">{props.title}</div>
+        <div className="title">{title}</div>
         <div className="content"></div>
         </div>
     );
